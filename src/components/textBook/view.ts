c8783a0ab9994b0c50b2ec5af4f9b873d5065d47
chagins <chagin_yav@hotmail.com>
2022-08-26 17:15:17 +0500

    feat: add textbook layout

--- src/components/textBook/view.ts
+++ src/components/textBook/view.ts
@@ -4,24 +4,84 @@ import { IWord } from '../types/types';
 export default class ViewTextBook {
   container: HTMLDivElement;
 
+  textBookContainer: HTMLDivElement;
+
+  headerContainer: HTMLDivElement;
+
+  pageContainer: HTMLDivElement;
+
+  paginationContainer: HTMLDivElement;
+
+  baseURL: string;
+
   constructor(container: HTMLDivElement) {
     this.container = container;
+    this.baseURL = 'https://rslang-learnwords-api.herokuapp.com';
+    this.textBookContainer = document.createElement('div');
+    this.textBookContainer.classList.add('textbook-container');
+    this.headerContainer = document.createElement('div');
+    this.headerContainer.classList.add('tb-header');
+    this.pageContainer = document.createElement('div');
+    this.pageContainer.classList.add('tb-page');
+    this.paginationContainer = document.createElement('div');
+    this.paginationContainer.classList.add('tb-pagination');
+    this.textBookContainer.append(this.headerContainer, this.pageContainer, this.paginationContainer);
+    this.container.append(this.textBookContainer);
   }
 
-  drawCard(word: IWord) {
-    const baseUrl = 'https://rslang-learnwords-api.herokuapp.com'; // temporary
+  draw({words, pageNumber, maxPageNumber}: {words: IWord[], pageNumber: number, maxPageNumber: number}) {
+    this.drawHeader();
+    this.drawPage(words);
+    this.drawPagination({pageNumber, maxPageNumber});
+  }
+
+  drawHeader() {
+    const tbGroupBtns = document.createElement('div');
+    tbGroupBtns.classList.add('tb-group-btns');
+    const btnsCount = 6;
+    for(let i = 0; i < btnsCount; i += 1) {
+      const btn = document.createElement('button');
+      btn.dataset.tbgroup = `${i}`;
+      btn.classList.add('tb-group-btn');
+      btn.innerText = `Группа ${i + 1}`
+      tbGroupBtns.appendChild(btn);
+    }
+    const btnHardGroup = document.createElement('button');
+    btnHardGroup.classList.add('tb-group-hardbtn');
+    btnHardGroup.innerText = `Сложные`;
+    tbGroupBtns.appendChild(btnHardGroup);
+
+    const btnGames = document.createElement('div');
+    btnGames.classList.add('tb-games-btns');
+    const btnSprintGame = document.createElement('button');
+    btnSprintGame.classList.add('tb-sprintgame-btn');
+    btnSprintGame.innerText = `🏃Спринт`;
+    const btnAudioGame = document.createElement('button');
+    btnAudioGame.classList.add('tb-audiogame-btn');
+    btnAudioGame.innerText = `🎧Аудиовызов`;
+    btnGames.append(btnSprintGame, btnAudioGame);
+    this.headerContainer.append(tbGroupBtns, btnGames);
+  }
+
+  drawPage(words: IWord[]) {
+    this.pageContainer.innerHTML = '';
+    words.forEach((word) => {
+      this.pageContainer.appendChild(this.getCard(word));
+    })
+  }
+
+  getCard(word: IWord): HTMLDivElement {
     const cardWord = document.createElement('div');
-    cardWord.dataset.wordid = word.id;
-    cardWord.classList.add('card-word');
-    this.container.appendChild(cardWord);
+    cardWord.dataset.tbwordid = word.id;
+    cardWord.classList.add('tb-card-word');
 
     cardWord.innerHTML = `
       <div class="main-wrapper">
-        <img class="word-picture" src="${baseUrl}/${word.image}" alt="${word.word}">
+        <img class="word-picture" src="${this.baseURL}/${word.image}" alt="${word.word}">
         <div class="card-content">
-          <audio src="${baseUrl}/${word.audio}" preload="auto"></audio>
-          <audio src="${baseUrl}/${word.audioMeaning}" preload="auto"></audio>
-          <audio src="${baseUrl}/${word.audioExample}" preload="auto"></audio>
+          <audio src="${this.baseURL}/${word.audio}" preload="auto"></audio>
+          <audio src="${this.baseURL}/${word.audioMeaning}" preload="auto"></audio>
+          <audio src="${this.baseURL}/${word.audioExample}" preload="auto"></audio>
           <div class="word-container">
             <span class="word-translate"><b>${word.word} - ${word.transcription} - ${word.wordTranslate}</b></span>
           </div>
@@ -48,5 +108,18 @@ export default class ViewTextBook {
           </div>
         </div>
       </div>`;
+    
+    return cardWord;
+  }
+
+  drawPagination({pageNumber, maxPageNumber}: {pageNumber: number, maxPageNumber: number}) {
+    this.paginationContainer.innerHTML = '';
+    const btnLeftPage = document.createElement('button');
+    btnLeftPage.classList.add('tb-leftpage-btn');
+    const pageNumberText = document.createElement('span');
+    pageNumberText.innerText = `${pageNumber} / ${maxPageNumber}`;
+    const btnRightPage = document.createElement('button');
+    btnRightPage.classList.add('tb-rightpage-btn');
+    this.paginationContainer.append(btnLeftPage, pageNumberText, btnRightPage);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,28 @@ const main = document.createElement('main') as HTMLDivElement;
 main.style.padding = '50px';
 document.body.appendChild(main);
 
-// wordsApi.getWords({ wordGroup: 0, wordPage: 0})
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => console.log(err));
-
 const wordID = '5e9f5ee35eb9e72bc21af4a0';
 
 const vb = new ViewTextBook(main);
+// vb.drawHeader();
 
-wordsApi
-  .getWord({ wordID })
+wordsApi.getWords({ wordGroup: 0, wordPage: 0})
   .then((res) => {
     console.log(res);
-    vb.drawCard(res);
+    const pageNumber = 1;
+    const maxPageNumber = 20;
+    vb.draw({words: res, pageNumber, maxPageNumber})
   })
   .catch((err) => console.log(err));
+
+
+
+// wordsApi
+//   .getWord({ wordID })
+//   .then((res) => {
+//     console.log(res);
+//     vb.drawCard({word: res});
+//   })
+//   .catch((err) => console.log(err));
 
 console.log('VICTORY');
