6baf53aa57914643059bf74e25963e7a6b618c07
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-09-02 00:55:08 +0300

    feat: add team page

--- src/components/about/view.ts
+++ src/components/about/view.ts
@@ -1,6 +1,6 @@
 export default class ViewAbout {
   drawTeamCard(
-    img: string,
+    i: number,
     name: string,
     github: string,
     role: string,
@@ -9,9 +9,9 @@ export default class ViewAbout {
   ) {
     const card = document.createElement('div');
 
-    card.className = 'team-card';
+    card.className = `team-card team-card_${i}`;
 
-    card.innerHTML = `<div class='team-card__img ${img}'>
+    card.innerHTML = `<div class='team-card__img team-card__img_${i}'></div>
                       <div class='team-card__content'>
                         <div class='team-card__main-info'>
                           <h4 class='team-card__name'>${name}</h4>
@@ -21,15 +21,16 @@ export default class ViewAbout {
                         </div>
                         <p class='team-card__header'>${role}</p>
                         <p class='team-card__paragraph'>${about}</p>
-                        <p class='team-card__header'>Что сделали?</p>
+                        <p class='team-card__header'>Что сделано?</p>
                         <p class='team-card__paragraph'>${work}</p>
                       </div>`;
     return card;
   }
 
   drawTeamCards(component: HTMLElement) {
+    const comp = component;
+    const mainWrapper = document.createElement('div');
     const teamCount = 3;
-    const teamImgs = ['1.png', '2.png', '3.png'];
     const teamNames = ['Ярослав', 'Ксения', 'Халид'];
     const teamGithub = [
       'https://github.com/chagins',
@@ -38,20 +39,28 @@ export default class ViewAbout {
     ];
     const teamRoles = ['Team Lead', 'Developer', 'Developer'];
     const teamAbout = [
-      'Я - слива лиловая, спелая, садовая!',
-      'А я - абрикос на юге pос!',
-      'А я томат!',
+      'Бэкенд разработчик, который хочет начать делать красивые вещи.',
+      'Менеджер проектов, который хочет руководить классами и переменными',
+      'Пожарный, который хочет спасать людей от форм сделанных на дивах.',
     ];
-    const teamWorks = ['Сделал то-то', 'Сделала то-то', 'Сделал то-то'];
+    const teamWorks = [
+      'Нэжно заставлял всех работать:) Базовая конфигурация проекта. Вся работа с бэком, включая запросы к серверу. Учебник. Массив слов для передачи в игры из учебника. Страница статистики.',
+      'Хедер. Главная страница. Страница "О нас". Логика связывания компонентов сайта (App). Игра "Аудиовызов". Экран выбора уровня сложности слов для игр, экран результатов игры.',
+      'Авторизация. Игра "Спринт". Обработка результатов игры и их отправка на сервер.'
+    ];
 
     const teamHeader = document.createElement('h3');
     teamHeader.innerHTML = 'Наша команда:';
-    component.append(teamHeader);
+    teamHeader.className = 'about__header';
+    mainWrapper.append(teamHeader);
+
+    mainWrapper.className = 'about-wrapper';
+    comp.className = 'about';
 
     for (let i = 0; i < teamCount; i += 1) {
-      component.append(
+      mainWrapper.append(
         this.drawTeamCard(
-          teamImgs[i],
+          i,
           teamNames[i],
           teamGithub[i],
           teamRoles[i],
@@ -60,6 +69,7 @@ export default class ViewAbout {
         )
       );
     }
-    document.body.append(component);
+    comp.append(mainWrapper)
+    document.body.append(comp);
   }
 }
