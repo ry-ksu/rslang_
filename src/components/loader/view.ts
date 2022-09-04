52ac97e5e34d24ac6c6f0ee92390f8fc0519245d
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-09-04 11:42:28 +0300

    feat: add loader

---
+++ src/components/loader/view.ts
@@ -0,0 +1,18 @@
+export default class ViewLoader {
+  drawLoader() {
+    const component = document.createElement('div');
+    component.className = 'loader__wrapper';
+    component.innerHTML = `<div class='loader'>
+                            <div class="loader__gif"></div>
+                          </div>`
+    document.body.after(component);
+  }
+
+  showLoader() {
+    (document.querySelector('.loader__wrapper') as HTMLElement).style.visibility = 'visible';
+  }
+
+  hideLoader() {
+    (document.querySelector('.loader__wrapper') as HTMLElement).style.visibility = 'hidden';
+  }
+}
